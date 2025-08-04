fb39cf852d7f6d01a54f024a11c8141358385996
Lyncon224 <lynconoliveira9710@gmail.com>
2025-08-03 21:03:05 -0300

    at

---
+++ TCC-E4u-L/src/pages/grafico.jsx
@@ -0,0 +1,37 @@
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "./../utils/data";
+import { GraficoBarra } from "../components/graficoBarra";
+import '../styles/pages/grafico.css'
+
+
+Chart.register(CategoryScale);
+
+export default function Grafico() {
+    
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+        {
+            label: "User Gaied",
+            data: Data.map((data) => data.userGain),
+            backgroundColor: [
+                'red',
+                'green',
+                'blue',
+                'grey',
+                'orange',
+                'pink',
+                'black',
+            ],
+            borderColor: "black",
+            borderWidth: 1
+        }
+    ]})
+
+  return (
+    <div>
+      <GraficoBarra chartData={chartData} />
+    </div>
+  )}
